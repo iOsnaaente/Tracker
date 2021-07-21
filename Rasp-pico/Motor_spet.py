0db68e5ec5ea00f04df380a594873b10bb123b48
iOsnaaente <bruno.bielsam.1205@hotmail.com>
2021-07-21 17:32:52 -0300

    Update 21-07-21

--- Rasp-pico/Motor_spet.py
+++ Rasp-pico/Motor_spet.py
@@ -1,98 +1,125 @@
-
-from time import sleep_ms
+from time    import sleep_ms
 from machine import Pin
+
+BACKWARD = False
+FORWARD  = True
+
+DISABLE = 1 
+ENABLE  = 0
 
 class Motor: 
     
-    direction = True
+    direction = FORWARD
+    torque    = ENABLE
+
     position  = 0.0 
-    torque    = False
+    ratio     = 1.0 
+    step      = 1.8
+    u_step    = 16 
+    nano_step = 0.0 
 
-    ratio     = 0.0 
-    step      = 0.0 
-    u_step    = 1
+    # Para ter 30rpm durante 1 segundo
+    rpm       = 0 
+    pps       = 800  
 
-    def __init__(self, step : int, dir : int, enable : int ) -> None:
-        self.pin_step       = Pin( step, Pin.OUT ) 
-        self.pin_direction  = Pin( dir, Pin.OUT )
-        self.pin_enable     = Pin( enable, Pin.OUT )
+    def __init__(self, STEP_PIN : int, DIRECTION_PIN : int, ENABLE_PIN : int, TORQUE : bool = False ) -> None:
+        self.STEP_PIN       = Pin( STEP_PIN, Pin.OUT ) 
+        self.DIRECTION_PIN  = Pin( DIRECTION_PIN, Pin.OUT )
+        self.ENABLE_PIN     = Pin( ENABLE_PIN, Pin.OUT )
+
+        self.direction      = FORWARD
+        self.torque         = ENABLE if TORQUE else DISABLE 
+        self.ENABLE_PIN.value( self.torque )
+        
+        self.step   = 1.8
+        self.u_step = 16 
+        self.ratio  = 1
+
+    def configure(self, step : float = 1.8, u_step : int = 16, ratio : float = 1) -> None: 
+        self.step   = step
+        self.u_step = u_step 
+        self.ratio  = ratio 
+
+    def set_torque( self, torque : bool = ENABLE ) -> None:
+        self.torque = ENABLE if torque else DISABLE 
+        self.ENABLE_PIN.value( self.torque )
+        
+
+    def set_velocity( self, rpm : int = 1 ) -> None:
+        self.pps = int( ( rpm * 360 * ( self.u_step // self.step ) )/( 60 * self.step ))
+        self.rpm = rpm 
     
-    def enable_torque( self, torque : bool ) -> None: 
-        self.pin_enable.value( not torque )
-        self.torque = torque 
-   
-    def _pulse(self, time_spend : int ) -> None :
-        self.pin_step.high()
+    def set_direction( self, direction : bool ) -> None:
+        self.direction = FORWARD if direction else BACKWARD
+        self.DIRECTION_PIN.value( 1 if self.direction == FORWARD else 0 ) 
+    
+    def _pulse(self, time_spend : int = 2 ) -> None :
+        self.STEP_PIN.high()
         sleep_ms ( time_spend//2 )
-        self.pin_step.low()
+        self.STEP_PIN.low()
         sleep_ms ( time_spend//2 )
-    
-    def direction( self, direction : bool ) -> None :
-        self.pin_direction.value( direction )
-        self.direction = direction 
-    
-    def set_direction( self, name : str ) -> None:
-        if name == 'FORWARD':
-            self.pin_direction.value( True ) 
-            self.direction = True 
-        elif name == 'BACKWARD':
-            self.pin_direction.value( False )
-            self.direction = False
+        
+    def move( self, angle : int ) -> None:  
+        n_pulso = (angle*self.u_step)/self.step
+        self.nano_step = n_pulso % 1
+        n_pulso = int( n_pulso // 1 )
+        if self.nano_step > 1.0 :
+            self.nano_step -= 1  
+            n_pulso += 1
+            
+        print( n_pulso ) 
+        if self.torque == DISABLE: 
+            self.ENABLE_PIN.low() 
+            for _ in range( n_pulso ):
+                self._pulse(  )
+            self.ENABLE_PIN.high() 
         else:
-            print("Parametro de direção inválido")
-
-    def move( self, num_pulses : int, vel : int = 1) -> None:
-        if not self.torque: 
-            for i in range( num_pulses ):
-                self.pin_enable.value( False )
-                self._pulse( vel )
-                self.pin_enable.value( True )
-        else:
-            for i in range( num_pulses ):
-                self._pulse( vel )
+            for i in range( n_pulso ):
+                self._pulse(  )
         
         if self.direction: self.position += self.step / (self.u_step * self.ratio)  
         else:              self.position -= self.step / (self.u_step * self.ratio)
-            
+
         if   self.position > 360.0: self.position -= 360.0
         elif self.position < 0:     self.position = 360 - self.position 
 
 
 class Motors:
-    GIR_POS = 0 
-    GIR_VEL = 0
+    GIR_POS = 0
     ELE_POS = 0 
-    ELE_VEL = 0 
 
-    def __init__(self, gir_stp : Pin, gir_dir : Pin, gir_enb : Pin, ele_stp : Pin, ele_dir : Pin, ele_enb : Pin ):
-        self.GIR = Motor( gir_stp, gir_dir, gir_enb )
-        self.ELE = Motor( ele_stp, ele_dir, ele_enb )
+    def __init__(self, gir_stp : Pin, gir_dir : Pin, ele_stp : Pin, ele_dir : Pin, enb_motors : Pin ):
+        self.GIR = Motor ( gir_stp, gir_dir, enb_motors )
+        self.ELE = Motor ( ele_stp, ele_dir, enb_motors )
     
     def configure(self, MOTOR : Motor, pos : float, step : float, micro_step : int, ratio : float ) -> None:
-        MOTOR.pos    = pos 
-        MOTOR.step   = step 
         MOTOR.u_step = micro_step 
         MOTOR.ratio  = ratio
+        MOTOR.step   = step 
+        MOTOR.pos    = pos 
 
-    def move(self, gir_num : int, gir_dir : bool, ele_num : int, ele_dir : bool ) :
-        self.GIR.set_direction( gir_dir )
-        self.ELE.set_direction( ele_dir )
-        for i in range( gir_num if gir_num > ele_num else ele_num ):
-            if ( gir_num > 0 ) :
-                self.GIR.move( 1 )
-                gir_num -= 1
-            if ( ele_num > 0 ) :
-                self.ELE.move( 1 )
-                ele_num -= 1
+    def set_torque(self, state : bool ) -> None :  
+        self.GIR.torque = ENABLE if state else DISABLE
+        self.GIR.ENABLE_PIN.value( self.GIR.torque )
+        self.ELE.torque = ENABLE if state else DISABLE
+        self.ELE.ENABLE_PIN.value( self.GIR.torque )
 
-    def enable_torque(self, name : str, state : bool ) -> None :
-        if name == 'GIR': 
-            self.GIR.enable_torque( state )
-        elif name == 'ELE':
-            self.ELE.enable_torque( state )
-        else:
-            print( 'torque_enable: Name not found ( use "Gir" or "ELE" )') 
+    def get_positions(self):
+        return [ self.GIR.position, self.ELE.position ]
+
+    def move(self, g_ang : int, e_ang : int ) -> None :
+        if   g_ang < 0:   self.GIR.set_direction( BACKWARD )
+        elif g_ang > 0:   self.GIR.set_direction( FORWARD  )
         
-    
+        if   e_ang < 0:   self.ELE.set_direction( BACKWARD ) 
+        elif e_ang > 0:   self.ELE.set_direction( FORWARD  )
+        
+        self.GIR.move( abs(g_ang) )
+        print( "Moveu o GIRO")
+        
+        self.ELE.move( abs(e_ang) )
+        print( "Moveu a ELEVAÇÃO")
+        
+        self.GIR_POS, self.ELE_POS = self.get_positions()
 
-
+        
--- Rasp-pico/main.py
+++ Rasp-pico/main.py
@@ -1,38 +1,74 @@
-from Motor_step import Motors
-DIR_MOTOR1    = 0 
-STEP_MOTOR1   = 1
-ENABLE_MOTOR1 = 2
-DIR_MOTOR2    = 3 
-STEP_MOTOR2   = 4
-ENABLE_MOTOR2 = 5
+from Motor_step  import Motors
+from I2C_RTC     import DS3231
+from time        import sleep
+from machine     import Pin
+from SunPosition import *
 
-MOTORS = Motors( STEP_MOTOR1, DIR_MOTOR1, ENABLE_MOTOR1, STEP_MOTOR2, DIR_MOTOR2, ENABLE_MOTOR2 )
+import select
+import struct 
+import sys
+import gc
+
+
+# VARIÁVEIS USADAS NAS GPIOs DO RASPICO 
+DIR_GIR     = 0
+DIR_ELE     = 1
+DIR_GEN     = 2 
+
+STEP_GIR    = 3 
+STEP_ELE    = 4 
+STEP_GEN    = 5
+
+BUTTON_GP   = 6 
+BUTTON_GM   = 7
+
+BUTTON_EP   = 8
+BUTTON_EM   = 9
+
+LED1_RED    = 10
+LED1_BLUE   = 11
+LED2_RED    = 12
+LED2_BLUE   = 13
+
+ENABLE_MTS  = 14
+
+NOT_USED    = 15
+
+SDA_DS      = 16 
+SCL_DS      = 17
+
+EE_SCK      = 18
+EE_DOO      = 19
+EE_DIO      = 20
+EE_CS       = 21
+
+LED_BUILTIN = 25
+
+# INSTANCIAMENTO DOS PERIFÉRICOS 
+MOTORS = Motors( STEP_GIR, DIR_GIR, STEP_ELE, DIR_ELE, ENABLE_MTS )
 MOTORS.configure( MOTORS.GIR, pos = 0.0, step = 1.8, micro_step = 16, ratio = 1000 )
 MOTORS.configure( MOTORS.ELE, pos = 0.0, step = 1.8, micro_step = 16, ratio = 1000 )
+MOTORS.set_torque( True )
 
+BUTTON_GP  = Pin( BUTTON_GP, Pin.IN, Pin.PULL_UP )
+BUTTON_GM  = Pin( BUTTON_GM, Pin.IN, Pin.PULL_UP )
+BUTTON_EP  = Pin( BUTTON_EP, Pin.IN, Pin.PULL_UP )
+BUTTON_EM  = Pin( BUTTON_EM, Pin.IN, Pin.PULL_UP )
 
-from machine import Pin
 LED1_RED   = Pin(10, Pin.OUT)
 LED1_BLUE  = Pin(11, Pin.OUT)
 LED2_RED   = Pin(12, Pin.OUT)
 LED2_BLUE  = Pin(13, Pin.OUT)
 
-# AVISO QUE ESTA LIGADO 
-LED1_BLUE.high()
-LED1_RED.low()
-LED2_BLUE.low()
-LED2_RED.low()
+LED_BUILTIN = Pin( LED_BUILTIN, Pin.OUT ) 
 
-
-from I2C_RTC import DS3231
 ADDR_DS = [0x68, 0x57] 
-SDA_DS = Pin( 16 ) 
-SCL_DS = Pin( 17 )
+SDA_DS = Pin( SDA_DS ) 
+SCL_DS = Pin( SCL_DS )
 DS = DS3231( 0, SDA_DS, SCL_DS, addrs = ADDR_DS )
-#DS.set_time( 21, 6, 24, 5, 11, 56, 3  )
+#DS.set_time( 21, 7, 20, 5, 18, 6, 30  )
 
 
-from SunPosition import * 
 LATITUDE  = -29.16530765942215
 LONGITUDE = -54.89831672609559 
 TIME      = [ 2021, 6, 23, 18, 6, 0, 3 ]
@@ -41,23 +77,66 @@ LOC       = [ LATITUDE, LONGITUDE, 298.5, 101.0 ]
 azimute = 0.0
 altitude = 0.0
 
-from time import sleep
-import select
-import struct 
-import sys
-import gc
+# AVISO QUE ESTA LIGADO 
+LED1_BLUE.high()
+LED1_RED.low()
+LED2_BLUE.low()
+LED2_RED.low()
 
 gc.enable()
 
-debug = False 
+debug = False
+
+import  _thread
+baton = _thread.allocate_lock()
+
+def pulse(pin):
+    pin.high()
+    sleep(0.001)
+    pin.low()
+    sleep(0.001)
+
+def motors_control( ):
+    while True:
+        flag = '' 
+        if BUTTON_GP.value() == 0:
+            MOTORS.move( 1, 0 )
+            LED1_BLUE.toggle()
+            flag =  "GIRO P "
+            
+        elif BUTTON_GM.value() == 0:
+            MOTORS.move(-1, 0 )
+            LED1_RED.toggle()
+            flag = "GIRO N "
+        
+        if BUTTON_EP.value() == 0:
+            MOTORS.move( 0, 1 )
+            LED2_BLUE.toggle()
+            flag = "ELEVAÇÃO P "
+        
+        elif BUTTON_EM.value() == 0:
+            MOTORS.move( 0,-1 )
+            LED2_RED.toggle()
+            flag = "ELEVAÇÃO N "
+            
+        if flag :
+            baton.acquire()
+            print( flag )
+            baton.release() 
+
+_thread.start_new_thread( motors_control, () ) 
 
 while True:
     TIME = DS.now()
      
-    sys.stdout.write( struct.pack('b', 99 ) )
-    sys.stdout.write( struct.pack('B', struct.calcsize('ffBBBBBBbB') ) )
-    sys.stdout.write( struct.pack('ff', azimute, altitude ) )
-    sys.stdout.write( struct.pack('BBBBBB', TIME[0], TIME[1], TIME[2], TIME[3], TIME[4], TIME[5] ) )
+    #sys.stdout.write( struct.pack('b', 99 ) )
+    #sys.stdout.write( struct.pack('B', struct.calcsize('ffBBBBBBbB') ) )
+    #sys.stdout.write( struct.pack('ff', azimute, altitude ) )
+    #sys.stdout.write( struct.pack('BBBBBB', TIME[0], TIME[1], TIME[2], TIME[3], TIME[4], TIME[5] ) )
+    
+    baton.acquire()
+    print( TIME, azimute, altitude )
+    baton.release() 
     
     while sys.stdin in select.select( [sys.stdin], [], [], 0 )[0]:        
         cmd = sys.stdin.read(1)
@@ -69,22 +148,19 @@ while True:
                 sys.stdout.write( 'Input = h : Correção da Hora' )
                 sys.stdout.write( 'Nova data/hora = %i/%i/%i - %i:%i:%i ' %(TIME[0],TIME[1],TIME[2],TIME[3],TIME[4],TIME[5]) ) 
         
-    # LED TOGGLE INDICATOR 
-    LED1_RED.toggle()
-    sleep(0.1)
-    LED1_RED.toggle()
-    sleep(0.9)
-    
     azimute, altitude = compute( LOC, TIME )
     
     LED2_BLUE.value ( 1 if altitude > 0 else 0 )
     LED2_RED.value  ( 0 if altitude > 0 else 1 )
     
     # SALVAR A DATA NA EEPROM DO RASP
-    # FAZER O PINO DE INTERRUPÇÃO CASO FALTE LUZ 
+    # FAZER O PINO DE INTERRUPÇÃO CASO FALTE LUZ
     
+    LED_BUILTIN.toggle() 
+    sleep( 1 ) 
     # GARBAGE COLLECTOR 
     gc.collect()
+
 
 
 
